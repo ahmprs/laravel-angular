01ac149ab8c5e880db074b966cdf0a2ada7708d2
parszadeh.ahmad <parszadeh.ahmad@gmail.com>
2020-03-12 23:37:33 +0330

    1398-12-23 0035 angular page refresh problem fixed by adding .htaccess file. File upload empty FILE array problem still not fixed.

--- frontend/src/app/backend-test/backend-test.component.ts
+++ frontend/src/app/backend-test/backend-test.component.ts
@@ -33,19 +33,25 @@ export class BackendTestComponent implements OnInit {
     );
   }
 
-  fileToUpload: File = null;
-  handleFileInput(files: FileList) {
-    this.fileToUpload = files.item(0);
+  // fileToUpload: File = null;
+  // handleFileInput(files: FileList) {
+  //   this.fileToUpload = files.item(0);
 
-    this.http.postGetJson(
-      "/api/upload",
-      { fileToUpload: this.fileToUpload },
-      d => {
-        console.log(d);
-      },
-      err => {
-        console.log(err);
-      }
-    );
+  //   this.http.postGetJson(
+  //     "/api/upload",
+  //     { fileToUpload: this.fileToUpload },
+  //     d => {
+  //       console.log(d);
+  //     },
+  //     err => {
+  //       console.log(err);
+  //     }
+  //   );
+  // }
+
+  fileToUpload: File = null;
+  fileChange(event) {
+    console.log(event);
+    this.fileToUpload = event[0];
   }
 }
